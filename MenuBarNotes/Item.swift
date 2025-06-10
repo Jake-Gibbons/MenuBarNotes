b9f1bd16d6ba679291f8e7c48f0f8cc4a84c3116
Jake Gibbons <jake_gibbons@live.co.uk>
2025-06-10 21:49:36 +0100

    Initial Commit

---
+++ MenuBarNotes/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MenuBarNotes
+//
+//  Created by Jake Gibbons on 10/06/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MenuBarNotesTests/MenuBarNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarNotesTests.swift
+//  MenuBarNotesTests
+//
+//  Created by Jake Gibbons on 10/06/2025.
+//
+
+import Testing
+@testable import MenuBarNotes
+
+struct MenuBarNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
